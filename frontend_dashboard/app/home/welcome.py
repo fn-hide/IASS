121bc3383d2dba722c8094d79c21ed4e8c046cf3
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2025-05-21 16:07:13 +0700

    💬 Resolve typo

--- frontend_dashboard/app/home/welcome.py
+++ frontend_dashboard/app/home/welcome.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Welcome",
-    page_icon=":material/login:",
+    page_icon=":material/waving_hand:",
     layout="wide",
     initial_sidebar_state="expanded",
 )
